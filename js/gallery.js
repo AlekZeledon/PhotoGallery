c3e151ce6696702a4e19120195e6577ba50d51b4
Alek Zeledon <alekzeledon@gmail.com>
2022-03-15 05:10:45 -0700

    popup windows

---
+++ js/gallery.js
@@ -0,0 +1,22 @@
+let galleryImages = document.querySelectorAll(".gallery-img");
+let getLatestOpenedImg;
+let windowWidth = window.innerWidth;
+
+if(galleryImages) {
+    galleryImages.forEach(function(image,index) {
+        image.onclick = function() {
+            let getElementCss = window.getComputedStyle(image);
+            let getFullImgUrl = getElementCss.getPropertyValue("background-image");
+            let getImgUrlPos = getFullImgUrl.split("/img/thumbnails/");
+            let setNewImgUrl = getImgUrlPos[1].replace('")', '');
+
+            getLatestOpenedImg = index + 1;
+
+            let container = document.body;
+            let newImgWindow = document.createElement("div");
+            container.appendChild(newImgWindow);
+            newImgWindow.setAttribute("class", "img-window");
+            newImgWindow.setAttribute("onclick", "closeImg()");
+        }
+    });
+}
