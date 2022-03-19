5211bb87a4498ca0fef50fecd5b2a2cf419681df
Alek Zeledon <alekzeledon@gmail.com>
2022-03-19 07:59:39 -0700

    added prev/next buttons

--- js/gallery.js
+++ js/gallery.js
@@ -21,10 +21,33 @@ if(galleryImages) {
             let newImg = document.createElement("img");
             newImgWindow.appendChild(newImg);
             newImg.setAttribute("src", "img/" + setNewImgUrl);
+
+            newImg.onload = function() {
+                let imgWidth = this.width;
+                let calcImgToEdge = ((windowWidth - imgWidth) / 2) - 80;
+
+                let newNextBtn = document.createElement("a");
+                let btnNextText = document.createTextNode("Next");
+                newNextBtn.appendChild(btnNextText);
+                container.appendChild(newNextBtn);
+                newNextBtn.setAttribute("class", "img-btn-next");
+                newNextBtn.setAttribute("onclick", "changeImg()");
+                newNextBtn.style.cssText = "right: " + calcImgToEdge + "px;";
+    
+                let newPrevBtn = document.createElement("a");
+                let btnPrevText = document.createTextNode("Prev");
+                newPrevBtn.appendChild(btnPrevText);
+                container.appendChild(newPrevBtn);
+                newPrevBtn.setAttribute("class", "img-btn-prev");
+                newPrevBtn.setAttribute("onclick", "changeImg()");
+                newPrevBtn.style.cssText = "left: " + calcImgToEdge + "px;";
+            }
         }
     });
 }
 
 function closeImg() {
     document.querySelector(".img-window").remove();
+    document.querySelector(".img-btn-next").remove();
+    document.querySelector(".img-btn-prev").remove();
 }
