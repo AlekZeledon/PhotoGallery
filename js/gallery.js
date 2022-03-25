e683d5229e042f956a915c281dc6f06647d64fb6
Alek Zeledon <alekzeledon@gmail.com>
2022-03-25 02:14:40 -0700

    fixed Next button

--- js/gallery.js
+++ js/gallery.js
@@ -84,10 +84,10 @@ function changeImg (changeDir) {
         let calcImgToEdge = ((windowWidth - imgWidth) / 2) - 80;
 
         let nextBtn = document.querySelector(".img-btn-next");
-        nextBtn.style.cssText = "right " + calcImgToEdge + "px;";
+        nextBtn.style.cssText = "right: " + calcImgToEdge + "px;";
 
         let prevBtn = document.querySelector(".img-btn-prev");
-        prevBtn.style.cssText = "left " + calcImgToEdge + "px;";
+        prevBtn.style.cssText = "left: " + calcImgToEdge + "px;";
     }
 
 
